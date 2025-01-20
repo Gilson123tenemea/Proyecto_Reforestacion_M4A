d27b65eec9339390604094e97dcc7b52e46915c7
Gilson123tenemea <tenemeaaguilar@gmail.com>
2025-01-19 19:42:04 -0500

    Subir proyecto

---
+++ src/main/java/com/example/demo/ProyectoReforestacionM4AApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoReforestacionM4AApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoReforestacionM4AApplication.class, args);
+	}
+
+}
