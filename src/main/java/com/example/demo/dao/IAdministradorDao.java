f22bd2128a1b4e9ea325a6b3d9f2f53ab2e93f51
DavidLop97 <167137212+DavidLop97@users.noreply.github.com>
2025-01-20 22:59:28 -0500

    Create IAdministradorDao.java
    AdministradorDao

---
+++ src/main/java/com/example/demo/dao/IAdministradorDao.java
@@ -0,0 +1,14 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import com.example.demo.entity.Administrador;
+
+public interface IAdministradorDao {
+
+	public List<Administrador> findAll();
+	public void save(Administrador administrador);
+	public Administrador findOne(Long id);
+	public void delete(Long id);
+
+}
