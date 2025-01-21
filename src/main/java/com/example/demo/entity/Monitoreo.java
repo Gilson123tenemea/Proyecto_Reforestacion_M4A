b510fa4eea6605773d9ba48f32e8ecf82c3d63b3
Mauricio <137722989+MauricioFajardo@users.noreply.github.com>
2025-01-20 23:05:00 -0500

    Create Monitoreo.java
    trabajo

---
+++ src/main/java/com/example/demo/entity/Monitoreo.java
@@ -0,0 +1,110 @@
+package com.example.demo.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Monitoreo")
+
+public class Monitoreo implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id_monitoreo;
+	private Long id_administrador;
+	private Long id_actividades;
+	private double altura_plantas;
+	private double duracion_monitoreo;
+	private String problemas_idetificados;
+	private String observaciones;
+	private String nota_administrador;
+	private Date fecha;
+	private String imagen;
+	private Long id_proyecto;
+	public Long getId_monitoreo() {
+		return id_monitoreo;
+	}
+	public void setId_monitoreo(Long id_monitoreo) {
+		this.id_monitoreo = id_monitoreo;
+	}
+	public Long getId_administrador() {
+		return id_administrador;
+	}
+	public void setId_administrador(Long id_administrador) {
+		this.id_administrador = id_administrador;
+	}
+	public Long getId_actividades() {
+		return id_actividades;
+	}
+	public void setId_actividades(Long id_actividades) {
+		this.id_actividades = id_actividades;
+	}
+	public double getAltura_plantas() {
+		return altura_plantas;
+	}
+	public void setAltura_plantas(double altura_plantas) {
+		this.altura_plantas = altura_plantas;
+	}
+	public double getDuracion_monitoreo() {
+		return duracion_monitoreo;
+	}
+	public void setDuracion_monitoreo(double duracion_monitoreo) {
+		this.duracion_monitoreo = duracion_monitoreo;
+	}
+	public String getProblemas_idetificados() {
+		return problemas_idetificados;
+	}
+	public void setProblemas_idetificados(String problemas_idetificados) {
+		this.problemas_idetificados = problemas_idetificados;
+	}
+	public String getObservaciones() {
+		return observaciones;
+	}
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
+	public String getNota_administrador() {
+		return nota_administrador;
+	}
+	public void setNota_administrador(String nota_administrador) {
+		this.nota_administrador = nota_administrador;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public String getImagen() {
+		return imagen;
+	}
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+	public Long getId_proyecto() {
+		return id_proyecto;
+	}
+	public void setId_proyecto(Long id_proyecto) {
+		this.id_proyecto = id_proyecto;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	
+	
+
+}
