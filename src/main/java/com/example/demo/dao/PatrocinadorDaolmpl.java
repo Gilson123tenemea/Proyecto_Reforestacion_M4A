6c7475f56e93322208a1e0172d8523f7f6799cd4
DavidLop97 <167137212+DavidLop97@users.noreply.github.com>
2025-01-20 23:00:27 -0500

    Create PatrocinadorDaolmpl.java
    Patrocinadorlmpl

---
+++ src/main/java/com/example/demo/dao/PatrocinadorDaolmpl.java
@@ -0,0 +1,43 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.Patrocinador;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Repository
+public class PatrocinadorDaolmpl implements IPatrocinadorDao{
+	
+	@PersistenceContext
+	private EntityManager em;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Patrocinador> findAll() {
+		return em.createQuery("from Patrocinador").getResultList();
+	}
+
+	@Override
+	public void save(Patrocinador patrocinador) {
+		if (patrocinador.getId_patrocinador() != null
+				&& patrocinador.getId_patrocinador() > 0) {
+			em.merge(patrocinador);
+		} else {
+			em.persist(patrocinador);
+		}
+	}
+
+	@Override
+	public Patrocinador findOne(Long id) {
+		return em.find(Patrocinador.class, id);
+	}
+
+	@Override
+	public void delete(Long id) {
+		em.remove(findOne(id));
+	}
+}
