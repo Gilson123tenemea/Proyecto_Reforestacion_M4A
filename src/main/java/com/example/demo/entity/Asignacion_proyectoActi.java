f34c6f067f0f3e831b1f181ab3afb2eb09337a69
Mariuxi <andreacalle888@gmail.com>
2025-01-20 22:24:00 -0500

    Create Asignacion_proyectoActi.java
    asignacion proyecto

---
+++ src/main/java/com/example/demo/entity/Asignacion_proyectoActi.java
@@ -0,0 +1,53 @@
+package com.example.demo.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name="asignacion_proyecto")
+public class Asignacion_proyectoActi implements Serializable {
+
+	
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id_asignacionproyecto;
+	@Column(name = "create_as")
+    @Temporal(TemporalType.DATE)
+    private Date createAs;
+
+    @PrePersist
+    public void prePersist() {
+        createAs = new Date();
+    }
+    
+    private static final long serialVersionUID = 3666176546640572827L;
+
+	public Long getId_asignacionproyecto() {
+		return id_asignacionproyecto;
+	}
+
+	public void setId_asignacionproyecto(Long id_asignacionproyecto) {
+		this.id_asignacionproyecto = id_asignacionproyecto;
+	}
+
+	public Date getCreateAs() {
+		return createAs;
+	}
+
+	public void setCreateAs(Date createAs) {
+		this.createAs = createAs;
+	}
+    
+    
+}
