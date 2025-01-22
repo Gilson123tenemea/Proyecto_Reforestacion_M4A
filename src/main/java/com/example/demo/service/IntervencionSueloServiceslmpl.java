787a4d4d50d0dd45ddcf729e14a07b558d588ca8
DavidLop97 <167137212+DavidLop97@users.noreply.github.com>
2025-01-21 21:14:44 -0500

    Create IntervencionSueloServiceslmpl.java
    Intervencionlmpl

---
+++ src/main/java/com/example/demo/service/IntervencionSueloServiceslmpl.java
@@ -0,0 +1,42 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.dao.IIntervencion_SueloDao;
+import com.example.demo.entity.Intervencion_Suelo;
+
+@Service
+public class IntervencionSueloServiceslmpl implements IIntervencionSueloServices{
+
+	@Autowired
+	private IIntervencion_SueloDao intervenciondao;
+	
+	@Transactional(readOnly = true)
+	@Override
+	public List<Intervencion_Suelo> findAll() {
+		return intervenciondao.findAll();
+	}
+
+	@Transactional
+	@Override
+	public void save(Intervencion_Suelo intervencion) {		
+		intervenciondao.save(intervencion);
+	}
+
+	@Transactional(readOnly = true)
+	@Override
+	public Intervencion_Suelo findOne(Long id) {
+		return intervenciondao.findOne(id);
+	}
+
+	@Transactional
+	@Override
+	public void delete(Long id) {	
+		intervenciondao.delete(id);
+	}
+
+}
