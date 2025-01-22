f7caf2db7153f1304aa1e67219d07a1e16686393
CARPONCHO10 <carpioalexander111@gmail.com>
2025-01-21 19:29:26 -0500

    Create ProyectoDaoImpl.java
    ProyectoDao

---
+++ src/main/java/com/example/demo/dao/ProyectoDaoImpl.java
@@ -0,0 +1,50 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.Actividades;
+import com.example.demo.entity.Proyecto;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Repository
+public class ProyectoDaoImpl implements IProyectoDao {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Proyecto> findAll() {
+		// TODO Auto-generated method stub
+		return em.createQuery("from Proyecto").getResultList();
+	}
+
+	@Override
+	public void save(Proyecto proyecto) {
+		if (proyecto.getId_proyecto() != null 
+				&& proyecto.getId_proyecto() > 0) {
+			em.merge(proyecto);
+		} else {
+			em.persist(proyecto);
+		}
+
+	}
+
+	@Override
+	public Proyecto findOne(Long id) {
+		// TODO Auto-generated method stub
+		return em.find(Proyecto.class, id);
+	}
+
+	@Override
+	public void delete(Long id) {
+		
+		em.remove(findOne(id));
+
+	}
+
+}
