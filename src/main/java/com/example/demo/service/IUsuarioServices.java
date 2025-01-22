3dbc5602a5280dec03bbf1db9069390fa7421a9c
DavidLop97 <167137212+DavidLop97@users.noreply.github.com>
2025-01-22 09:57:17 -0500

    Create IUsuarioServices.java
    UsuarioService

---
+++ src/main/java/com/example/demo/service/IUsuarioServices.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.entity.Usuarios;
+
+
+public interface IUsuarioServices {
+	public List<Usuarios> findAll();
+	public void save(Usuarios usuario);
+	public Usuarios findOne(Long id);
+	public void delete(Long id);
+
+}
