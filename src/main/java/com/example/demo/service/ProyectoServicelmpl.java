4d9b51d5cd9724b8cc7ad865438ccdb17af4aa9a
DavidLop97 <167137212+DavidLop97@users.noreply.github.com>
2025-01-21 21:46:28 -0500

    Create ProyectoServicelmpl.java
    Proyectolmpl

---
+++ src/main/java/com/example/demo/service/ProyectoServicelmpl.java
@@ -0,0 +1,42 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.dao.IProyectoDao;
+import com.example.demo.entity.Proyecto;
+
+@Service
+public class ProyectoServicelmpl implements IProyectoServices{
+
+	@Autowired
+	private IProyectoDao proyectodao;
+	
+	@Transactional(readOnly = true)
+	@Override
+	public List<Proyecto> findAll() {
+		return proyectodao.findAll();
+	}
+
+	@Transactional
+	@Override
+	public void save(Proyecto proyecto) {	
+		proyectodao.save(proyecto);
+	}
+
+	@Transactional(readOnly = true)
+	@Override
+	public Proyecto findOne(Long id) {
+		return proyectodao.findOne(id);
+	}
+
+	@Transactional
+	@Override
+	public void delete(Long id) {
+		proyectodao.delete(id);
+	}
+
+}
