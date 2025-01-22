4a0f576555e8a2fcd0be6c00788ef48c023a43e3
DavidLop97 <167137212+DavidLop97@users.noreply.github.com>
2025-01-22 09:57:26 -0500

    Update UsuariosServiceImpl.java
    Usuariolmpl

--- src/main/java/com/example/demo/service/UsuariosServiceImpl.java
+++ src/main/java/com/example/demo/service/UsuariosServiceImpl.java
@@ -1,5 +1,42 @@
 package com.example.demo.service;
 
-public class UsuariosServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.dao.IUsuariosDao;
+import com.example.demo.entity.Usuarios;
+
+@Service
+public class UsuariosServiceImpl implements IUsuarioServices{
+	
+	@Autowired
+	private IUsuariosDao usuariosDao;
+
+	@Transactional(readOnly=true)
+	@Override
+	public List<Usuarios> findAll() {
+		return usuariosDao.findAll();
+	}
+
+	@Transactional
+	@Override
+	public void save(Usuarios usuario) {	
+		usuariosDao.save(usuario);
+	}
+
+	@Transactional(readOnly=true)
+	@Override
+	public Usuarios findOne(Long id) {
+		return usuariosDao.findOne(id);
+	}
+
+	@Transactional
+	@Override
+	public void delete(Long id) {
+		usuariosDao.delete(id);
+	}
 
 }
