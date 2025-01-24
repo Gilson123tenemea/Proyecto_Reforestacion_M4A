3839317bd8da04f6e991ba2b3d4f701f9b6b19da
DavidLop97 <167137212+DavidLop97@users.noreply.github.com>
2025-01-23 20:06:05 -0500

    Provincias
    EliminarListar

--- src/main/java/com/example/demo/controllers/ProvinciaControllers.java
+++ src/main/java/com/example/demo/controllers/ProvinciaControllers.java
@@ -1,13 +1,14 @@
 package com.example.demo.controllers;
 
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.entity.Provincia;
 import com.example.demo.service.IProvinciaService;
@@ -43,5 +44,16 @@ public class ProvinciaControllers {
         model.addAttribute("titulo", "Lista de Provincias");
         model.addAttribute("provincias", provinciaService.findAll());
         return "listarProvincias";
+    }
+    
+    @GetMapping("/provincias/eliminar/{id}")
+    public String eliminarProvincia(@PathVariable Long id, RedirectAttributes attributes) {
+        try {
+            provinciaService.delete(id);
+            attributes.addFlashAttribute("mensaje", "Provincia eliminada correctamente");
+        } catch (Exception e) {
+            attributes.addFlashAttribute("error", "Error al eliminar la provincia");
+        }
+        return "redirect:/listarProvincias";
     }
 }
