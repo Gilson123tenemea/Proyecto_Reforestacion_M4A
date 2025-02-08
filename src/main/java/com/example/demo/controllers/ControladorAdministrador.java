cde636d818cf9a230e44701ba092a86bfe6dbfd4
FreddyGomezO <freddy.gomez.est@tecazuay.edu.ec>
2025-02-08 01:11:34 -0500

    arreglo administrador

--- src/main/java/com/example/demo/controllers/ControladorAdministrador.java
+++ src/main/java/com/example/demo/controllers/ControladorAdministrador.java
@@ -17,10 +17,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -28,7 +26,6 @@ import com.example.demo.entity.Administrador;
 import com.example.demo.entity.Canton;
 import com.example.demo.entity.Parroquia;
 import com.example.demo.entity.Provincia;
-import com.example.demo.entity.SuperAdministrador;
 import com.example.demo.entity.Usuarios;
 import com.example.demo.service.IAdministradorServices;
 import com.example.demo.service.ICantonService;
@@ -45,174 +42,112 @@ public class ControladorAdministrador {
 	
 	@Autowired
 	private IUsuarioServices usuarioServices;
-	  @Autowired
-	    private IProvinciaService provinciaService;
+	
+	@Autowired
+	private IProvinciaService provinciaService;
 
-	    @Autowired
-	    private ICantonService cantonService;
+	@Autowired
+	private ICantonService cantonService;
 
-	    @Autowired
-	    private IParroquiaService parroquiaService;
+	@Autowired
+	private IParroquiaService parroquiaService;
 
-	    @InitBinder
-	    public void initBinder(WebDataBinder binder) {
-	        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-	        dateFormat.setLenient(false);
-	        binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, true));
-	    }
-
-	   // @GetMapping("/testProvincias")
-	    @ResponseBody
-	    public List<Provincia> testProvincias() {
-	        return provinciaService.findAll(); // Verifica si esto devuelve datos
-	    }
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		dateFormat.setLenient(false);
+		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, true));
+	}
 
 	@GetMapping("/listarAdministradores")
 	public String listarAdministradores(Model model) {
-	    model.addAttribute("titulo", "Lista de Administradores");
+		model.addAttribute("titulo", "Lista de Administradores");
 
-	    List<Administrador> administradores = administradorServices.findAll();
-	    List<Usuarios> usuarios = usuarioServices.findAll();
+		List<Administrador> administradores = administradorServices.findAll();
+		List<Usuarios> usuarios = usuarioServices.findAll();
 
-	    List<Map<String, Object>> combinados = new ArrayList<>();
+		List<Map<String, Object>> combinados = new ArrayList<>();
 
-	    for (Usuarios usuario : usuarios) {
-	        if (!usuario.getAdministrador().isEmpty()) {
-	            Administrador administrador = usuario.getAdministrador().get(0); 
-	            Map<String, Object> datosCombinados = new HashMap<>();
-	            datosCombinados.put("administrador", administrador);
-	            datosCombinados.put("usuario", usuario);
-	            combinados.add(datosCombinados);
-	        }
-	    }
+		for (Usuarios usuario : usuarios) {
+			if (!usuario.getAdministrador().isEmpty()) {
+				Administrador administrador = usuario.getAdministrador().get(0); 
+				Map<String, Object> datosCombinados = new HashMap<>();
+				datosCombinados.put("administrador", administrador);
+				datosCombinados.put("usuario", usuario);
+				combinados.add(datosCombinados);
+			}
+		}
 
-	    model.addAttribute("combinados", combinados);
-	    return "listarAdministradores";
+		model.addAttribute("combinados", combinados);
+		return "listarAdministradores";
 	}
 
-//	@RequestMapping(value = "/administrador", method = RequestMethod.GET)
+	@GetMapping("/administrador")
 	public String listarYEditar(@RequestParam(value = "id", required = false) Long id, Map<String, Object> model, RedirectAttributes flash) {
-	    Administrador administrador = new Administrador();
-	    Usuarios usuario = new Usuarios(); // Inicializa el objeto usuario
-
-	    if (id != null && id > 0) {
-	        administrador = administradorServices.findOne(id);
-	        if (administrador != null) {
-	            usuario = usuarioServices.findOne(administrador.getId_usuarios());
-	        } else {
-	            flash.addFlashAttribute("info", "El administrador no existe en la base de datos");
-	            return "redirect:/administrador";
-	        }
-	    }
-
-	    model.put("administrador", administrador);
-	    model.put("usuario", usuario); 
-	    
-	    model.put("titulo", "Editar o Crear Administrador");
-	    return "administrador";
+		Administrador administrador = new Administrador();
+		Usuarios usuario = new Usuarios();
+		
+		if (id != null && id > 0) {
+			administrador = administradorServices.findOne(id);
+			if (administrador != null) {
+				usuario = usuarioServices.findOne(administrador.getId_usuarios());
+			} else {
+				flash.addFlashAttribute("info", "El administrador no existe en la base de datos");
+				return "redirect:/listarAdministradores";
+			}
+		}
+		
+		model.put("administrador", administrador);
+		model.put("usuario", usuario);
+		model.put("provincias", provinciaService.findAll());
+		model.put("titulo", "Editar o Crear Administrador");
+		
+		return "administrador"; // Cambia a la vista del formulario
 	}
-	
-	
-    @GetMapping("/inicioadmin")
-    public String iniciosuperadmin(Model model) {
-        model.addAttribute("titulo", "Inicio SuperAdmin");
-        return "inicioadmin";
-    }
-
 
 	@PostMapping("/guardar")
 	public String guardarAdministradorYUsuario(
-	        @ModelAttribute("administrador") Administrador administrador,
-	        @ModelAttribute("usuario") Usuarios usuario,
-	        @ModelAttribute SuperAdministrador superdamin,
-	        Model model) {
-		   Long idSuperAdministrador = (Long) model.asMap().get("idSuperAdministrador");
-		   if (idSuperAdministrador != null) {
-			   superdamin.setId_super_administrador(idSuperAdministrador);
-	        } else {
-	            System.out.println("ID del patrocinador es null"); 
-	        }
-	    try {
-	    	if (usuario.getId_usuarios() != null) {
-	    	    Usuarios usuarioExistente = usuarioServices.findOne(usuario.getId_usuarios());
-	    	    if (usuarioExistente != null) {
-	    	        usuarioExistente.setCedula(usuario.getCedula());
-	    	        usuarioExistente.setNombre(usuario.getNombre());
-	    	        usuarioExistente.setApellido(usuario.getApellido());
-	    	        usuarioExistente.setCorreo(usuario.getCorreo());
-	    	        
-	    	        // Aquí se asegura de que el ID de la parroquia se esté estableciendo
-                    usuarioExistente.setId_parroquia(usuario.getId_parroquia());
-                    
-                    if (usuario.getFecha_nacimiento() != null) {
-                        usuarioExistente.setFecha_nacimiento(usuario.getFecha_nacimiento());
-                    }
-	    	        usuarioExistente.setGenero(usuario.getGenero());
-	    	        usuarioExistente.setCelular(usuario.getCelular());
-	    	        usuarioExistente.setContraseña(usuario.getContraseña());
-	    	        usuario = usuarioExistente;
-	    	    }
-	    	}
+			@ModelAttribute("administrador") Administrador administrador,
+			@ModelAttribute("usuario") Usuarios usuario,
+			Model model) {
+		try {
+			// Actualiza el usuario existente
+			if (usuario.getId_usuarios() != null) {
+				Usuarios usuarioExistente = usuarioServices.findOne(usuario.getId_usuarios());
+				if (usuarioExistente != null) {
+					usuarioExistente.setCedula(usuario.getCedula());
+					usuarioExistente.setNombre(usuario.getNombre());
+					usuarioExistente.setApellido(usuario.getApellido());
+					usuarioExistente.setCorreo(usuario.getCorreo());
+					usuarioExistente.setId_parroquia(usuario.getId_parroquia());
+					usuarioExistente.setFecha_nacimiento(usuario.getFecha_nacimiento());
+					usuarioExistente.setGenero(usuario.getGenero());
+					usuarioExistente.setCelular(usuario.getCelular());
+					usuarioExistente.setContraseña(usuario.getContraseña());
+					usuario = usuarioExistente;
+				}
+			}
 
-	    	usuarioServices.save(usuario);
-	        administrador.setId_usuarios(usuario.getId_usuarios());
-	        administradorServices.save(administrador);
-	        return "redirect:/listarAdministradores";
-	        
-	    } catch (Exception e) {
-	        model.addAttribute("mensaje", "Error al guardar: " + e.getMessage());
-	        return "error";
-	    }
+			usuarioServices.save(usuario);
+			administrador.setId_usuarios(usuario.getId_usuarios());
+			administradorServices.save(administrador);
+			return "redirect:/listarAdministradores";
+
+		} catch (Exception e) {
+			model.addAttribute("mensaje", "Error al guardar: " + e.getMessage());
+			return "error";
+		}
 	}
 
-
-
-	@RequestMapping("/administrador")
-	public String crear(Map<String, Object> model) {
-	    model.put("administrador", new Administrador());
-	    model.put("usuarios", usuarioServices.findAll());
-	    model.put("provincias", provinciaService.findAll());
-	    model.put("usuario", new Usuarios()); // Asegúrate de que esto esté aquí
-	    return "administrador"; 
+	@GetMapping("/admin/cantones/{idProvincia}")
+	@ResponseBody
+	public List<Canton> getCantonesByProvincia(@PathVariable Long idProvincia) {
+		return cantonService.findByProvincia(idProvincia);
 	}
-   
-    
-    @PostMapping("/delete/{id}")
-    public String deleteAdministrador(@PathVariable Long id, Model model) {
-        try {
-            Administrador administrador = administradorServices.findOne(id);
 
-            if (administrador == null) {
-                model.addAttribute("mensaje", "Administrador no encontrado con ID: " + id);
-                return "error";
-            }
-
-            administradorServices.delete(id);
-            model.addAttribute("mensaje", "Administrador eliminado exitosamente");
-            return "redirect:/listarAdministradores";
-        } catch (Exception e) {
-            model.addAttribute("mensaje", "Error al eliminar el administrador: " + e.getMessage());
-            return "error";
-        }
-    }
-    
-    
-    @GetMapping("/provincias")
-    @ResponseBody
-    public List<Provincia> getProvincias() {
-        return provinciaService.findAll();
-    }
-
-    @GetMapping("/admin/cantones/{idProvincia}")
-    @ResponseBody
-    public List<Canton> getCantonesByProvincia(@PathVariable Long idProvincia) {
-        return cantonService.findByProvincia(idProvincia);
-    }
-
-    @GetMapping("/admin/parroquias/{idCanton}")
-    @ResponseBody
-    public List<Parroquia> getParroquiasByCanton(@PathVariable Long idCanton) {
-        return parroquiaService.findByCanton(idCanton);
-    }
-
+	@GetMapping("/admin/parroquias/{idCanton}")
+	@ResponseBody
+	public List<Parroquia> getParroquiasByCanton(@PathVariable Long idCanton) {
+		return parroquiaService.findByCanton(idCanton);
+	}
 }
